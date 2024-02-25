a582a9c0a6c0fa46a08af8d8669ac23a75d334cf
oussama <oussamalm147@gmail.com>
2024-02-25 15:09:30 +0100

    ignore unnecessary folders

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,10 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller'; // Import your UsersController
 import { UsersService } from './users/users.service'; // Import your UsersService
+import { AnimalsModule } from './animals/animals.module';
+import { AdoptionController } from './adoption/adoption.controller';
+import { AdoptionService } from './adoption/adoption.service';
+import { AdoptionModule } from './adoption/adoption.module';
+import { ReviewsController } from './reviews/reviews.controller';
+import { ReviewsService } from './reviews/reviews.service';
+import { PhotosController } from './photos/photos.controller';
+import { PhotosService } from './photos/photos.service';
+import { PhotosModule } from './photos/photos.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, UsersController], // Add your UsersController to the controllers array
-  providers: [AppService, UsersService], // Add your UsersService to the providers array
+  imports: [AnimalsModule, AdoptionModule, PhotosModule],
+  controllers: [AppController, UsersController, AdoptionController, ReviewsController, PhotosController], // Add your UsersController to the controllers array
+  providers: [AppService, UsersService, AdoptionService, ReviewsService, PhotosService], // Add your UsersService to the providers array
 })
 export class AppModule {}
