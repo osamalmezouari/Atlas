ed12e731b8192d4a2ec1a14eff7e39d935960ff8
oussama <oussamalm147@gmail.com>
2024-02-27 23:16:28 +0100

    add some images

---
+++ db/data.js
@@ -0,0 +1,7 @@
+module.exports = {
+  img1: 'https://images.unsplash.com/photo-1641761963569-d7ec2843315e?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  img2: '',
+  img3: 'https://images.unsplash.com/photo-1494256997604-768d1f608cac?q=80&w=1858&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  img4: 'https://images.unsplash.com/photo-1603277160434-df7471138363?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  img5: 'https://themebeyond.com/beyond/billu/wp-content/uploads/2023/03/H2B.jpg'
+};
