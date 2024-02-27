9acbcb9c32e5ca531c9ffc299afbcdd52a54f839
oussama <oussamalm147@gmail.com>
2024-02-27 23:17:49 +0100

    add data file for store chnage the static data in the future

--- db/data.js
+++ db/data.js
@@ -1,7 +1,9 @@
 module.exports = {
   img1: 'https://images.unsplash.com/photo-1641761963569-d7ec2843315e?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-  img2: '',
+  img2: 'https://images.unsplash.com/photo-1634115503749-0d2b50fe4caa?q=80&w=1890&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   img3: 'https://images.unsplash.com/photo-1494256997604-768d1f608cac?q=80&w=1858&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   img4: 'https://images.unsplash.com/photo-1603277160434-df7471138363?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-  img5: 'https://themebeyond.com/beyond/billu/wp-content/uploads/2023/03/H2B.jpg'
+  img5: 'https://themebeyond.com/beyond/billu/wp-content/uploads/2023/03/H2B.jpg',
+  img6:'https://images.unsplash.com/photo-1641761957161-c53206e6dcd4?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  img7:"https://images.unsplash.com/photo-1623704835217-cca5bf3e0228?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 };
