4b355f89ccd4a9657acede5645606cbf962af0ac
oussama <oussamalm147@gmail.com>
2024-03-07 15:22:25 +0100

    *

--- client/src/pages/App.tsx
+++ client/src/pages/App.tsx
@@ -2,12 +2,15 @@ import '../styles/App.css'
 // import Landing from "./landing.tsx";
 // import Login from "./login.tsx";
 // import Register from "./register.tsx";
-import Adoption_list_box from "../components/adoption_list_box.tsx";
+// import Adoption_list_box from "../components/adoption_list_box.tsx";
+// import Landing from "./landing.tsx";
+// import Adoption_list_box from "../components/adoption_list_box.tsx";
+import Landing from "./landing.tsx";
 
 function App() {
   return (
     <div>
-      <Adoption_list_box />
+      <Landing />
     </div>
   )
 }
