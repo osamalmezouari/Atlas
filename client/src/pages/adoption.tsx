c2198fbd301dbc89b2290a88109dd74d496267e3
oussama <oussamalm147@gmail.com>
2024-03-11 00:25:34 +0000

    fix text size for the responsive and move the searchbar to the top

--- client/src/pages/adoption.tsx
+++ client/src/pages/adoption.tsx
@@ -9,10 +9,10 @@ export default function Adoption() {
             <Navbar />
             <div className={'grid grid-rows-[500px,auto,1fr] '}>
                 <div className={'pathcontainer  bg-path bg-center bg-cover relative'}>
-                    <div className={'relative px-16 flex items-center m-auto top-44'}>
+                    <div className={'relative px-16 flex items-center m-auto top-44 max-md:px-1'}>
                         <div>
                             <p
-                              className={"pathname bg-orange text-6xl font-custom font-bold text-white p-3 rounded-xl mb-4 "}>
+                              className={"pathname bg-orange text-[9vh] font-custom font-bold text-white px-3 rounded-xl mb-4 "}>
                                 Adoption
                             </p>
                             <p className={"fullpath flex gap-x-3"}>
@@ -23,7 +23,7 @@ export default function Adoption() {
                     </div>
 
                 </div>
-                <div className={""}>
+                <div className={"relative -top-16"}>
                     <div
                       className={"searchbar row-start-1 w-9/12 z-50 pb-5 p-2 grid grid-rows-[auto,1fr] mx-auto text-white rounded-2xl mr-auto max-md:m-0 max-md:w-full"}>
                     <Searchbar />
