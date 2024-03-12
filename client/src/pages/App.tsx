cd8f2b52dec78ea327600a60b9219c3ee5abf33b
oussama <oussamalm147@gmail.com>
2024-03-12 17:57:35 +0000

    *

--- client/src/pages/App.tsx
+++ client/src/pages/App.tsx
@@ -1,15 +1,10 @@
 import '../styles/App.css'
-// import Adoption from "./adoption.tsx";
-// import Landing from "./landing.tsx";
-// import Leave_review from "./leave_review.tsx";
-// import Adoption from "./adoption.tsx";
-// import Landing from "./landing.tsx";
-import Adoption from "./adoption.tsx";
+import Personal from "./user/personal.tsx";
 
 function App() {
   return (
     <div>
-      <Adoption />
+      <Personal />
     </div>
   )
 }
--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -7,7 +7,7 @@ function Login() {
         <div className={'relative flex justify-center items-center h-screen max-md:pt-20 max-md:p-2 ' }>
           <form className={"bg-rev bg-contain grid grid-rows w-[450px] h-[450px] rounded-xl p-4 hover:shadow-2xl transition-all duration-1000 border-orange border-2 "}>
             {/*<div className={'bg-about w-[140px] h-[140px] rounded-full absolute top-20 flex items-center justify-center '}><div className={'bg-logo bg-cover bg-center border-2 border-orange w-[125px] h-[125px] rounded-full  '}></div></div>*/}
-            <input className={"row-start-2 pl-2 outline-none h-12 border-2  hover:border-dbleu duration-500 transition-all rounded"} type={"email"} placeholder={"Username or Email"}></input>
+            <input className={"row-start-2 pl-2 outline-none h-12 border-2  hover:border-dbleu duration-500 transition-all rounded"} type={"email"} placeholder={"Email"}></input>
             <input className={"row-start-3 pl-2 outline-none h-12 border-2  hover:border-dbleu duration-500 transition-all rounded"} type={"password"} placeholder={"Password"}></input>
             <button className={'capitalize  border-2 rounded text-dbleu hover:text-white hover:bg-orange transition-all duration-500 border-orange h-12 row-start-5 bg-findshap bg-contain'} type={'submit'}> login</button>
             <div className={'row-start-4'}>
