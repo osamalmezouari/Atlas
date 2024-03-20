e80c9cfbdf6a9169486ba596cae79e9d4ccee2a9
oussama <oussamalm147@gmail.com>
2024-03-20 21:40:36 +0000

    Fix create and update functionality for review.module.ts

--- server/src/reviews/review.module.ts
+++ server/src/reviews/review.module.ts
@@ -3,9 +3,10 @@ import { ReviewService } from "./review.service";
 import { ReviewController } from "./review.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Review } from "./entities/review.entity";
+import { UsersModule } from "../users/users.module";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Review])],
+  imports: [TypeOrmModule.forFeature([Review]), UsersModule],
   providers: [ReviewService],
   controllers: [ReviewController],
 })
