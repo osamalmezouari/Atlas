42ec081895e8bddb259205fbd47e28ce405c73b2
oussama <oussamalm147@gmail.com>
2024-03-20 00:43:33 +0000

    Implement initial setup of functionality for server/src/reviews/review.service.ts

--- server/src/reviews/review.service.ts
+++ server/src/reviews/review.service.ts
@@ -3,6 +3,7 @@ import * as mysql from "mysql";
 import { Review } from "./entities/review.entity";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
+import { CreateReviewDto } from "./dto/create-review.dto";
 
 @Injectable()
 export class ReviewService {
@@ -15,23 +16,13 @@ export class ReviewService {
     return this.ReviewsRepository.find();
   }
 
-  // create(review: Review): Promise<void> {
-  //   return new Promise((resolve, reject) => {
-  //     this.connection.query(
-  //       "INSERT INTO reviews SET ?",
-  //       review,
-  //       (error, results, fields) => {
-  //         if (error) {
-  //           reject(error);
-  //           return;
-  //         }
-  //         resolve();
-  //       },
-  //     );
-  //   });
-  // }
+  create(createreviewdto: CreateReviewDto): Promise<Review> {
+    console.log(createreviewdto);
+    this.ReviewsRepository.create(createreviewdto);
+    return this.ReviewsRepository.save(createreviewdto);
+  }
 
-  async delete(id: number) {
+  async delete(id: String) {
     const review = await this.ReviewsRepository.findOne({ where: { id } });
     if (!review) {
       throw new Error(`review with the id = ${id} ${HttpStatus.NOT_FOUND}`);
