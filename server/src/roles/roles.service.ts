929a3d59caa2c55c6c03fde84ca424ca3450849f
oussama <oussamalm147@gmail.com>
2024-03-20 00:43:34 +0000

    Implement initial setup of functionality for server/src/roles/roles.service.ts

--- server/src/roles/roles.service.ts
+++ server/src/roles/roles.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from "@nestjs/common";
+import { HttpStatus, Injectable } from "@nestjs/common";
 import { Repository } from "typeorm";
 import { Role } from "./entities/roles.entity";
 import { InjectRepository } from "@nestjs/typeorm";
+import { UpdateRoleDto } from "./dto/update-role.dto";
 
 @Injectable()
 export class RolesService {
@@ -13,12 +14,36 @@ export class RolesService {
   findAll(): Promise<Role[]> {
     return this.rolesrepository.find();
   }
+  async findOne(id: String): Promise<Role> {
+    return await this.rolesrepository.findOne({ where: { id } });
+  }
+  async create(createroledto) {
+    this.rolesrepository.create({
+      ...createroledto,
+    });
+    return this.rolesrepository.save({
+      ...createroledto,
+    });
+  }
+  async update(id: String, updateroledto: UpdateRoleDto) {
+    console.log(id);
+    const exsitingRole = await this.rolesrepository.preload({
+      id,
+      ...updateroledto,
+    });
+    if (!exsitingRole) {
+      throw new Error(
+        `no role with given id ${id} is match top update ${HttpStatus.NOT_FOUND}`,
+      );
+    }
+    return this.rolesrepository.save({
+      id,
+      ...updateroledto,
+    });
+  }
 
-  // create(){
-  //   return this.rolerepository.create()
-  // }
-
-  async delete(id: number) {
-    const role = this.rolesrepository.findOne({ where: { id } });
+  async delete(id: String) {
+    const role = await this.rolesrepository.findOne({ where: { id } });
+    return this.rolesrepository.remove(role);
   }
 }
