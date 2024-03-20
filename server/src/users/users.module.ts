8620c604909150b5f43c8cd9e314351467a84023
oussama <oussamalm147@gmail.com>
2024-03-20 21:40:37 +0000

    Fix create and update functionality for users.module.ts

--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -3,10 +3,12 @@ import { UsersController } from "./users.controller";
 import { UsersService } from "./users.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { User } from "./entities/user.entity";
+import { RolesModule } from "../roles/roles.module";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), RolesModule],
   providers: [UsersService],
   controllers: [UsersController],
+  exports: [UsersService],
 })
 export class UsersModule {}
