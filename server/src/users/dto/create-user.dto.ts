13f15aad0eb2f1766215935a9bfe744ec32da23a
oussama <oussamalm147@gmail.com>
2024-03-20 21:40:37 +0000

    Fix create and update functionality for create-user.dto.ts

--- server/src/users/dto/create-user.dto.ts
+++ server/src/users/dto/create-user.dto.ts
@@ -1,14 +1,8 @@
-import { Column } from "typeorm";
-
 export class CreateUserDto {
-  @Column({ nullable: false })
   firstname: String;
-  @Column({ nullable: false })
   lastname: String;
-  @Column({ nullable: false })
   email: String;
-  @Column({ nullable: false })
   password: String;
-  @Column({ default: true })
   active: Boolean;
+  role: String;
 }
