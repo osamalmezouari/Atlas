7c2ad5e2c1048a5f0b36eca991631e1aa0f31e66
oussama <oussamalm147@gmail.com>
2024-03-20 21:40:36 +0000

    Fix create and update functionality for review.service.ts

--- server/src/reviews/review.service.ts
+++ server/src/reviews/review.service.ts
@@ -4,22 +4,32 @@ import { Review } from "./entities/review.entity";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { CreateReviewDto } from "./dto/create-review.dto";
+import { UsersService } from "../users/users.service";
 
 @Injectable()
 export class ReviewService {
   constructor(
     @InjectRepository(Review)
     private readonly ReviewsRepository: Repository<Review>,
+    private readonly userService: UsersService,
   ) {}
 
   findAll(): Promise<Review[]> {
-    return this.ReviewsRepository.find();
+    return this.ReviewsRepository.find({
+      relations: {
+        user: true,
+      },
+    });
   }
 
-  create(createreviewdto: CreateReviewDto): Promise<Review> {
-    console.log(createreviewdto);
-    this.ReviewsRepository.create(createreviewdto);
-    return this.ReviewsRepository.save(createreviewdto);
+  async create(createReviewDto: CreateReviewDto): Promise<Review> {
+    const { userId, ...reviewData } = createReviewDto;
+    const user = await this.userService.findOne(userId);
+    const newReview = this.ReviewsRepository.create({
+      ...reviewData,
+      user: user,
+    });
+    return this.ReviewsRepository.save(newReview);
   }
 
   async delete(id: String) {
