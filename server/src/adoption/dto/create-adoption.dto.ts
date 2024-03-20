4e352315c1351f285b65868fef9d519dddf83444
oussama <oussamalm147@gmail.com>
2024-03-20 21:40:35 +0000

    Fix create and update functionality for create-adoption.dto.ts

--- server/src/adoption/dto/create-adoption.dto.ts
+++ server/src/adoption/dto/create-adoption.dto.ts
@@ -1,29 +1,15 @@
-import { Column, PrimaryGeneratedColumn } from "typeorm";
-import { Image } from "../entities/images.entity";
-
 export class CreateAdoptionDto {
-  @Column({ nullable: false })
   posted_date: Date;
-  @Column({ default: false })
   adopted: Boolean;
-  @Column({ nullable: false })
   animal_name: String;
-  @Column({ nullable: false })
   animal_race: String;
-  @Column({ nullable: false })
   animal_age: Number;
-  @Column({ nullable: false })
   animal_gender: String;
-  @Column({ nullable: false })
   animal_class: String;
-  @Column({ nullable: false })
   description: String;
-  @Column({ nullable: true })
   weight: Number;
-  @Column({ nullable: false })
   health: String;
-  @Column({ nullable: false })
   castrate: Boolean;
-  @Column({ nullable: false })
+  user: String;
   image: String[];
 }
