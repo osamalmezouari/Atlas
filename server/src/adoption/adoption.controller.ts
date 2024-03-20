06ee210c5a9e4bb7fa165cd8c6be1057c3cc788d
oussama <oussamalm147@gmail.com>
2024-03-20 00:43:32 +0000

    Implement initial setup of functionality for server/src/adoption/adoption.controller.ts

--- server/src/adoption/adoption.controller.ts
+++ server/src/adoption/adoption.controller.ts
@@ -10,6 +10,7 @@ import {
 import { AdoptionService } from "./adoption.service";
 import { Adoption } from "./entities/adoption.entity";
 import { CreateAdoptionDto } from "./dto/create-adoption.dto";
+import { UpdateAdoptionDto } from "./dto/update-adoption.dto";
 
 @Controller("adoption")
 export class AdoptionController {
@@ -21,22 +22,25 @@ export class AdoptionController {
   }
 
   @Get(":id")
-  findById(@Param("id") id: string) {
-    return this.adoptionService.findOne(+id);
+  findById(@Param("id") id: String) {
+    return this.adoptionService.findOne(id);
   }
 
-  // @Post()
-  // create(@Body() createAdoptionDto: CreateAdoptionDto) {
-  //   return this.adoptionService.create(createAdoptionDto);
-  // }
-  //
-  // @Put(":id")
-  // update(@Param("id") id: string, @Body() updatedAdoption: any): Promise<void> {
-  //   return this.adoptionService.update(+id, updatedAdoption);
-  // }
-  //
+  @Post()
+  async create(@Body() createAdoptionDto: CreateAdoptionDto) {
+    return await this.adoptionService.create(createAdoptionDto);
+  }
+
+  @Put(":id")
+  async update(
+    @Param("id") id: String,
+    @Body() updatedAdoption: UpdateAdoptionDto,
+  ) {
+    return await this.adoptionService.update(id, updatedAdoption);
+  }
+
   @Delete(":id")
-  delete(@Param("id") id: string) {
-    return this.adoptionService.delete(+id);
+  delete(@Param("id") id: String) {
+    return this.adoptionService.delete(id);
   }
 }
