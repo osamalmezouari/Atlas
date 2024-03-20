708c8321c5867112f99c2543ae57280d6f1c0a6b
oussama <oussamalm147@gmail.com>
2024-03-20 21:40:35 +0000

    Fix create and update functionality for adoption.module.ts

--- server/src/adoption/adoption.module.ts
+++ server/src/adoption/adoption.module.ts
@@ -5,9 +5,10 @@ import { AdoptionController } from "./adoption.controller";
 import { AdoptionService } from "./adoption.service";
 import { Adoption } from "./entities/adoption.entity";
 import { Image } from "./entities/images.entity";
+import { UsersModule } from "../users/users.module";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Adoption, Image])],
+  imports: [TypeOrmModule.forFeature([Adoption, Image]), UsersModule],
   controllers: [AdoptionController],
   providers: [AdoptionService],
 })
