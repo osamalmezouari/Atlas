13d0da945d7e8c6d942b87b38166151e24dd0100
oussama <oussamalm147@gmail.com>
2024-03-24 23:17:06 +0000

    init

--- client/src/features/adoption/adoptionSlice.ts
+++ client/src/features/adoption/adoptionSlice.ts
@@ -1,8 +1,14 @@
 import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
 import  axios from "axios";
 
+export const findFilteredAdoptions = createAsyncThunk('adoption/findFilteredAdoptions', async ({classParam , locationParam } :{classParam ?:string,locationParam ?:string}) => {
+    return axios.get(classParam && locationParam ? `http://localhost:3000/Adoptions/${classParam}/${locationParam}`:`http://localhost:3000/Adoptions`)
+        .then((res)=>res.data)
+        .catch((error)=> console.log(error))
+});
+
 export const findAllAdoptions = createAsyncThunk('adoption/findAll', async () => {
-    return axios.get('http://localhost:3000/Adoptions')
+    return axios.get(`http://localhost:3000/Adoptions`)
         .then((res)=>res.data)
         .catch((error)=> console.log(error))
 });
@@ -13,11 +19,31 @@ export const findOneAdoption = createAsyncThunk('adoption/findOne',async (Adopti
         .catch((err)=>console.log(err))
 })
 
+export const findAllLocations = createAsyncThunk(
+    '',async () => {
+        return axios.get('http://localhost:3000/Adoptions/locations')
+            .then((res)=>res.data)
+            .catch((err)=>console.log(err))
+    }
+)
+
 const initialState = {
-    loading : false,
-    Adoptions: [],
-    OneAdoption : {},
-    Error : ""
+    Adoptions: {
+        data : [],
+        loading : false,
+        Error:""
+    },
+    OneAdoption : {
+        data : {},
+        loading : false,
+        Error : ""
+    },
+    FilteredAdoptions : {
+        data : [],
+        loading : false,
+        Error:""
+    },
+    AllLocations : [],
 }
 
 const AdoptionSlice = createSlice({
@@ -29,34 +55,58 @@ const AdoptionSlice = createSlice({
     extraReducers : (builder)=>{
         builder
             .addCase(findAllAdoptions.pending,(state)=>{
-                state.loading = true
-                state.Adoptions = []
-                state.Error = ""
+                state.Adoptions.loading = true
+                state.Adoptions.data = []
+                state.Adoptions.Error = ""
             })
             .addCase(findAllAdoptions.fulfilled,(state,action)=>{
-                state.loading = false
-                state.Adoptions = action.payload
-                state.Error = ""
+                state.Adoptions.loading = false
+                state.Adoptions.data =action.payload
+                state.Adoptions.Error = ""
             })
             .addCase(findAllAdoptions.rejected,(state)=>{
-                state.loading = false
-                state.Adoptions = []
-                state.Error = "something was failed"
+                state.Adoptions.loading = true
+                state.Adoptions.data = []
+                state.Adoptions.Error = "something go wrong"
             })
             .addCase(findOneAdoption.pending,(state)=>{
-                state.loading = true
-                state.OneAdoption = []
-                state.Error = ""
+                state.OneAdoption.loading = true
+                state.OneAdoption.data = []
+                state.OneAdoption.Error = ""
             })
             .addCase(findOneAdoption.fulfilled,(state,action)=>{
-                state.loading = false
-                state.OneAdoption= action.payload
-                state.Error = ""
+                state.OneAdoption.loading = false
+                state.OneAdoption.data= action.payload
+                state.OneAdoption.Error = ""
             })
             .addCase(findOneAdoption.rejected,(state)=>{
-                state.loading = false
-                state.OneAdoption = {}
-                state.Error = "there is an error"
+                state.OneAdoption.loading = false
+                state.OneAdoption.data = {}
+                state.OneAdoption.Error = "there is an error"
+            })
+            .addCase(findFilteredAdoptions.pending,(state)=>{
+                state.FilteredAdoptions.loading = true
+                state.FilteredAdoptions.data = []
+                state.FilteredAdoptions.Error = ""
+            })
+            .addCase(findFilteredAdoptions.fulfilled,(state,action)=>{
+                state.FilteredAdoptions.loading = false
+                state.FilteredAdoptions.data = action.payload
+                state.FilteredAdoptions.Error = ""
+            })
+            .addCase(findFilteredAdoptions.rejected,(state)=>{
+                state.FilteredAdoptions.loading = false
+                state.FilteredAdoptions.data = []
+                state.FilteredAdoptions.Error = "there is an error"
+            })
+            .addCase(findAllLocations.pending,(state)=>{
+                state.AllLocations = []
+            })
+            .addCase(findAllLocations.fulfilled,(state,action)=>{
+                state.AllLocations = action.payload
+            })
+            .addCase(findAllLocations.rejected,(state)=>{
+                state.AllLocations = []
             })
     }
 })
