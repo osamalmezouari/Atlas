3993a8a61d429c8a93518fd0b492d346136bc768
oussama <oussamalm147@gmail.com>
2024-03-25 23:43:43 +0000

    add the user state type

---
+++ client/Types/user.ts
@@ -0,0 +1,11 @@
+export interface User{
+    firstname: string;
+    lastname: string;
+    email: string;
+    password: string;
+    active: boolean;
+    facebook: string;
+    WhatsApp: string;
+    phone: string;
+    role: string;
+}
