825b9ffe27f8bbf56ad84b6d076b60bfa530d35f
oussama <oussamalm147@gmail.com>
2024-03-25 23:40:58 +0000

    Modified server/src/main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { CorsMiddleware } from "../cors.middleware";
-// import { ValidationPipe } from "@nestjs/common";
+// import { ValidationPipe } from "@nestjs/Types";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
