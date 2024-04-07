edc57925425ff22e5b1b938c20d0f910484ecdd8
oussama <oussamalm147@gmail.com>
2024-04-07 00:53:08 +0000

    enable cros fro port 5173

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import { CorsMiddleware } from "../cors.middleware";
-// import { ValidationPipe } from "@nestjs/Types";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(new CorsMiddleware().use);
+  app.enableCors({
+    origin: "http://localhost:5173",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
